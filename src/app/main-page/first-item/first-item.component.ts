93b7d97ce543b6b16eafad7f981a9c2843b6f4b0
Feyel-rnd <113343892+Feyel-rnd@users.noreply.github.com>
2022-10-06 09:28:02 +0200

    2 files modified

--- src/app/main-page/first-item/first-item.component.ts
+++ src/app/main-page/first-item/first-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as Realm from 'realm-web';
 
 @Component({
   selector: 'app-first-item',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./first-item.component.css']
 })
 export class FirstItemComponent implements OnInit {
-
+  
+  //console.log("Dog", Dog);
   constructor() { }
 
   ngOnInit() {
+
+  const app = new Realm.App('data-icqqg');
+  const mongo = app.currentUser.mongoClient('Cluster0');
+  const collection = mongo.db('Users').collection("Example ID");
+  collection.find({state:true}).then((value)=>{
+    console.log(value)
+  })
+  
+
   }
 
 }
